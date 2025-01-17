0b38f02ccd866de6ed830d63b9863e2b2cdc00af
Gorantla Krishna <gorantla.krishna@tredence.com>
2025-01-17 17:11:43 +0530

    update

---
+++ Reflection Agent/chains.py
@@ -0,0 +1,31 @@
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+
+reflection_prompt = ChatPromptTemplate.from_messages(
+  [
+    (
+        "System",
+        "You are a viral twitter influencer grading a tweet.Generate critique and recommendations for the user's tweet",
+        "Always provide detailed recommendations,including requests for length,virality,style, etc. "
+    ),
+    MessagesPlaceholder(variable_name = "messages"),
+  ]
+)
+
+generation_prompt = ChatPromptTemplate.from_messages(
+  [
+    (
+        "System",
+        "You are a twitter techie influencer assistant tasked with writing excellent twitter posts.",
+        "Generate the best twitter post possible for the users request",
+        "If the users provide critique,respond with a revised version of your previous attempts",
+    ),
+    MessagesPlaceholder(variable_name="messages")
+  ]
+)
+
+llm = ChatOpenAI()
+
+generate_chain = generation_prompt | llm
+reflect_chain = reflection_prompt | llm
+
